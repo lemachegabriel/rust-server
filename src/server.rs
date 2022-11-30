2ae5a7d5f9ed94c5d6bf7e4473f9bfe2608e0b07
lemachegabriel <lemache35@gmail.com>
2022-11-29 22:41:02 -0300

    server runing

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::{net::TcpListener, io::Read};
 pub struct Server {
   addr: String,
 }
@@ -10,6 +11,23 @@ impl Server {
   }
 
   pub fn run(self) {
-    println!("Listening on {}", self.addr )
+    println!("Listening on {}", self.addr );
+
+    let listener = TcpListener::bind(&self.addr).unwrap();
+
+    loop {
+      match listener.accept() {
+        Ok((mut stream, _)) => {
+          let mut buffer = [0; 1024];
+          match stream.read(&mut buffer) {
+            Ok(_) => {
+              println!("Recived a request: {}", String::from_utf8_lossy(&buffer))
+            }
+            Err(e) => println!("Failed to read from conection: {}", e)
+          }
+        },  
+        Err(e) => println!("Failed to establish conection {}", e)
+      }
+    }
   }
 }
